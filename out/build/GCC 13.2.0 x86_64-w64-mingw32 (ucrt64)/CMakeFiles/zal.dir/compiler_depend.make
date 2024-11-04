556e6c5cbaa7582e592ac631067a03debd2087ff
Łukasz Rybakowski <rybakowski2001@gmail.com>
2024-11-04 20:51:51 +0100

    new file:   .vscode/tasks.json 	new file:   CMakePresets.json 	new file:   ObjCounter.hpp 	new file:   Statki.hpp 	new file:   Stocznia.hpp 	new file:   catch.hpp 	modified:   transportujFlota.hpp

---
+++ out/build/GCC 13.2.0 x86_64-w64-mingw32 (ucrt64)/CMakeFiles/zal.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for zal.
+# This may be replaced when dependencies are built.
--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -1,9 +1,24 @@
 #pragma once
 
 #include "Stocznia.hpp"
+#include "ObjCounter.hpp"
+#include "Statki.hpp"
 
-unsigned int transportujFlota(unsigned int towar)
-{
-    // Twoja implementacja tutaj
-    return 0;
+// Funkcja transportująca towary za pomocą floty statków
+unsigned int transportujFlota(unsigned int towar) {
+    if (towar == 0) return 0;  // Jeżeli towar do transportu wynosi 0, nie twórz żadnego statku
+
+    Stocznia stocznia;  // Tworzymy fabrykę statków
+    Stocznia::resetTotalCap();  // Resetujemy całkowitą pojemność przed rozpoczęciem transportu
+    CountThis<Zaglowiec>::reset();  // Resetujemy licznik żaglowców przed rozpoczęciem transportu
+
+    unsigned int transported = 0;  // Licznik przetransportowanego towaru
+
+    while (transported < towar) {
+        Statek* statek = stocznia();  // Tworzymy nowy statek za pomocą fabryki
+        transported += statek->transportuj();  // Dodajemy ilość towaru przetransportowaną przez ten statek
+        delete statek;  // Usuwamy utworzony statek
+    }
+
+    return CountThis<Zaglowiec>::get();  // Zwracamy liczbę żaglowców użytych do transportu
 }
